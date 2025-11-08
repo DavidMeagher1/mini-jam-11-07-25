4ffb0fc849236283cae08a4cb011b8af686d0250
DavidMeagher1 <meagherjdavid@gmail.com>
2025-11-08 14:53:32 -0800

    refactor(InventoryItem): remove debug print statements

--- HUD/Inventory/InventoryItem.gd
+++ HUD/Inventory/InventoryItem.gd
@@ -13,13 +13,10 @@ func set_item_data(data: ItemData) -> void:
 		texture_normal = null
 
 func _on_pressed() -> void:
-	print_debug(Game.active_item)
 	if Game.active_item == null:
 		Game.grab_item(item_data, true)
-		print_debug("Grabbed %s from inventory" % item_data.name)
 		accept_event()
 	else:
-		print_debug("Trying to craft %s with %s" % [Game.active_item.name, item_data.name])
 		var recipe_output = Game.active_item.get_crafted_item(item_data)
 		if recipe_output:
 			Game.consume_active_item()
