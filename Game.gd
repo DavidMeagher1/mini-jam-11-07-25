07b10aa21681587497f9b3c215a2132ce319ae60
Leonard Meagher <LeonardMeagher2@gmail.com>
2025-11-08 07:18:45 -0800

    feat(drag-and-drop): implement drag preview functionality for inventory items

--- Game.gd
+++ Game.gd
@@ -1,7 +1,19 @@
-extends Node
+extends Control
 
 var inventory: InventoryData = InventoryData.new()
 
 func _ready() -> void:
-    inventory.add_item(load("uid://o1t7t4wuynpc"))
-    inventory.add_item(load("uid://bfnq3rjfl7phv"))
+	inventory.add_item(load("uid://o1t7t4wuynpc"))
+
+func make_drag_preview(item_data: ItemData) -> Control:
+	var preview = CenterContainer.new()
+	preview.size = Vector2.ZERO
+	var texture_container = TextureRect.new()
+	preview.add_child(texture_container)
+	texture_container.stretch_mode = TextureRect.STRETCH_KEEP
+	texture_container.texture = item_data.icon
+	return preview
+
+func force_drag_item(item: ItemData) -> void:
+	var drag_preiew = make_drag_preview(item)
+	force_drag(item, drag_preiew)
--- Inventory/InventoryItem.gd
+++ Inventory/InventoryItem.gd
@@ -2,14 +2,6 @@ extends TextureRect
 
 @export var item_data: ItemData: set = set_item_data
 
-func make_drag_preview() -> Control:
-	var preview = CenterContainer.new()
-	preview.size = Vector2.ZERO
-	var texture_container = TextureRect.new()
-	preview.add_child(texture_container)
-	texture_container.stretch_mode = TextureRect.STRETCH_KEEP
-	texture_container.texture = item_data.icon
-	return preview
 
 func set_item_data(data: ItemData) -> void:
 	item_data = data
@@ -20,5 +12,5 @@ func set_item_data(data: ItemData) -> void:
 
 func _get_drag_data(_at_position: Vector2) -> Variant:
 	Game.inventory.remove_item(item_data)
-	set_drag_preview(make_drag_preview())
+	set_drag_preview(Game.make_drag_preview(item_data))
 	return item_data
