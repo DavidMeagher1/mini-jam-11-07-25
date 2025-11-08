eb2305dc5da4c24b27857b6e1a91ecb290b1ea9d
Leonard Meagher <LeonardMeagher2@gmail.com>
2025-11-08 15:55:29 -0800

    refactor(ItemData): simplify crafting logic by using get method for recipes

--- Inventory/ItemData.gd
+++ Inventory/ItemData.gd
@@ -3,10 +3,7 @@ class_name ItemData extends Resource
 @export var name: String = ""
 @export var icon: Texture2D
 # we are input a, input b -> output
-@export var recipes: Dictionary[ItemData,ItemData]
-
-func can_craft_with(item: ItemData) -> bool:
-    return recipes.has(item)
+@export var recipes: Dictionary[ItemData, ItemData]
 
 func get_crafted_item(item: ItemData) -> ItemData:
-    return recipes[item] if can_craft_with(item) else null
+    return recipes.get(item, item.recipes.get(self, null))
