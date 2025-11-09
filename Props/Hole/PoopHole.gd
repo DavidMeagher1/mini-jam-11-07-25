45682fb8c6ef2e580dcaec11987da18578f9ff08
DavidMeagher1 <meagherjdavid@gmail.com>
2025-11-09 13:40:30 -0800

    feat(Hole): add Flowers interaction and update item handling logic

--- Props/Hole/PoopHole.gd
+++ Props/Hole/PoopHole.gd
@@ -1,9 +1,20 @@
 extends Sprite2D
 
-
+var flowers_item: ItemData = load("uid://c60inoqd4l6c") # Flowers
+@onready var flowers: Interactable = $Flowers
 func _on_interactable_clicked(button: int) -> void:
     if Game.active_item == null:
         return
     if Game.active_item.name == "StrangeSeed":
         Game.consume_active_item()
-        pass # TODO PLANT SEED
+        flowers.show()
+        flowers.disabled = false
+
+
+func _on_flowers_clicked(button: int) -> void:
+    if not Game.active_item == null:
+        return
+    Game.grab_item(flowers_item)
+    flowers.hide()
+    flowers.disabled = true
+    
