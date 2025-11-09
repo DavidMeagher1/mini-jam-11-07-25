5ba717529810b9766c40fbece458080753a51074
DavidMeagher1 <meagherjdavid@gmail.com>
2025-11-09 10:39:38 -0800

    feat(ItemContainer): add random item selection logic for item grabbing

--- Props/ItemContainer.gd
+++ Props/ItemContainer.gd
@@ -1,6 +1,7 @@
 class_name ItemContainer extends Interactable
 
 @export var items: Array[ItemData] = []
+@export var random:bool = false
 
 func _ready() -> void:
     clicked.connect(_on_clicked)
@@ -8,6 +9,13 @@ func _ready() -> void:
 func _on_clicked() -> void:
     if Game.active_item:
         return
-    var item = items.pop_front()
-    if item:
-        Game.grab_item(item)
+    if not random:
+        var item = items.pop_front()
+        if item:
+            Game.grab_item(item)
+    else:
+        var index = randi_range(0, items.size())
+        var item = items[index]
+        items.remove_at(index)
+        if item:
+            Game.grab_item(item)
