1bb4afa0e9dfd7a49ea3ebb6518073c51e32f36d
Leonard Meagher <LeonardMeagher2@gmail.com>
2025-11-09 14:28:08 -0800

    feat(Murderer): add area entered signal handling to start timer

--- Murderer/Muderer.gd
+++ Murderer/Muderer.gd
@@ -13,10 +13,6 @@ func _ready() -> void:
 	set_physics_process(false)
 	hide()
 
-func _on_mouse_entered() -> void:
-	if timer:
-		timer.start()
-
 func _physics_process(delta: float) -> void:
 	if held:
 		shake(delta)
@@ -70,3 +66,8 @@ func _on_too_loud() -> void:
 		%murderer.play("Love")
 		if %Knife:
 			%Knife.queue_free()
+
+
+func _on_face_area_entered(_area: Area2D) -> void:
+	if timer:
+		timer.start()
